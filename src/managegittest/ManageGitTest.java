91ae777ea46cf7341bd3ab2bda6f5ffbe527527b
Kurnia Sandy Nugraha <42927559+Endorsijahad@users.noreply.github.com>
2018-09-28 19:14:54 +0700

    Call getName()

--- src/managegittest/ManageGitTest.java
+++ src/managegittest/ManageGitTest.java
@@ -15,9 +15,9 @@ public class ManageGitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(getName());
     }
-    public String getName(){
+    public static String getName(){
         return "Icha";
     }
     
