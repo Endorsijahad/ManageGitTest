15477f1a97332b290e16b1f968214e17fe4c818e
Kurnia Sandy Nugraha <42927559+Endorsijahad@users.noreply.github.com>
2018-09-28 19:45:52 +0700

    Kosongin main

--- src/managegittest/ManageGitTest.java
+++ src/managegittest/ManageGitTest.java
@@ -15,8 +15,6 @@ public class ManageGitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(getName());
-        System.out.println(getName());
     }
     public static String getName(){
         return "Nande";
