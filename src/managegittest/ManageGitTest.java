bfa077a2759c99c0e65eb8acccdf34697095e26a
Kurnia Sandy Nugraha <42927559+Endorsijahad@users.noreply.github.com>
2018-09-28 19:46:24 +0700

    Nampilin "aku anak bangsat"

--- src/managegittest/ManageGitTest.java
+++ src/managegittest/ManageGitTest.java
@@ -15,6 +15,7 @@ public class ManageGitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("aku anak bangsat");
     }
     public static String getName(){
         return "Nande";
